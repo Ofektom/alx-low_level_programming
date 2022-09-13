6e329b091eb141a3600ba579d72e3076c9e76a1e
Ofofonono Okpoho <okpohot@gmail.com>
2022-09-13 06:37:46 -0700

    Task 2 - Functions and Loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - Prints lower case alphabet 10 times
+ */
+void print_alphabet_x10(void)
+{
+char letter;
+int num;
+
+for (num = 1; num <= 10; num++)
+{
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
+}
+}
