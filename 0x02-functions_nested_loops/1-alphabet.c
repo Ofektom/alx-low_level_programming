4b7f81de91e623feba7131e71a8a7f28a419b370
Ofofonono Okpoho <okpohot@gmail.com>
2022-09-13 05:58:42 -0700

    Task 1 - Function -Print_Alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * main - Prints lower case alphabets
+ * print_alphabet - Prints lower case alphabets
  *
- * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
