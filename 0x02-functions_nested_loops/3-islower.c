17f5beef5d226d60777c46ae20489f74c05ddcc3
Ofofonono Okpoho <okpohot@gmail.com>
2022-09-13 07:08:00 -0700

    Task 3 - islower function

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_islower -checks for lower case characters
+ *
+ *Return: 1 if it is lower and 0 it it is not
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+
+	return (1);
+else 
+	return (0);
+}
