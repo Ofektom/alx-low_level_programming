881c2a3ba2136b394414d7c274fd7c2ea066992a
Ofofonono Okpoho <okpohot@gmail.com>
2022-09-15 21:11:13 -0700

    Task 6 - Absolut number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - Function for absolute number
+ * @r: Integer inout
+ * Desc: This function returns absolute value of a number
+ * Return: Absolut value of r
+ */
+int _abs(int r)
+{
+if (r >= 0)
+	return (r);
+else
+	return (r * -1);
+}
