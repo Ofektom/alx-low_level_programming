92f5c9d4fb23798ab7fb46d7bc16661da7280151
Ofofonono Okpoho <okpohot@gmail.com>
2022-09-20 18:29:37 -0700

    Task 3 - Standard output of string characters

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - Prints string to standard output
+ * @str: input string
+ * Return: 0
+ */
+void _puts(char *str)
+{
+int n = 0;
+
+while (str[n] != '\0')
+{
+	_putchar(str[n]);
+	n++;
+}
+return;
+}
