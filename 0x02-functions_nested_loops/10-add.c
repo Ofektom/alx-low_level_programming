a6a9ac0728833add881758a658cc23af8fc51fce
Ofofonono Okpoho <okpohot@gmail.com>
2022-09-26 02:09:41 -0700

    Task 10 - adds 2 numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - adds 2 numbers
+ * @a: first integer input
+ * @b: second integer input
+ * Return: sum of the 2 numbers
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
