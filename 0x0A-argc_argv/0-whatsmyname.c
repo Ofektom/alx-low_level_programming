c2729c3916c591121fed21ef9ba8d2f01fa94951
Ofofonono Okpoho <okpohot@gmail.com>
2022-10-02 16:07:40 -0700

    Task 0 - Print name of file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Prints its name then a newling
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if successful
+ */
 int main(int argc, char *argv[])
 {
 	if (argc > 0)
-		printf("%s\n",argv[0]);
+		printf("%s\n", argv[0]);
 	return (0);
 }
