dedc12b5296fcbb4ebd6c1f3c6a480a8a004b00f
Ofofonono Okpoho <okpohot@gmail.com>
2022-10-02 13:48:10 -0700

    Task 0 - Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,11 +1,17 @@
 #include "main.h"
 
+/**
+ * _puts_recursion - prints string to stdout
+ * @s: input string
+ * Return: nothing
+ */
 void _puts_recursion(char *s)
 {
-if (*s)
+if (*s == '\0')
+	_putchar('\n');
+else
 {
 	_putchar(*s);
 	_puts_recursion(s + 1);
 }
-_putchar('\n');
 }
