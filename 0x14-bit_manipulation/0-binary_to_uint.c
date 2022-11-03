7de00f39d7efd9ec05df734e2f47864558c84f33
Ofofonono Okpoho <okpohot@gmail.com>
2022-11-03 04:30:21 -0700

    Bit manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * binary_to_uint - vonverts binary number to decimal
+ * @b: Binary input
+ * Return: decimal equivalent
+ */
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int base = 1, result = 0, len = 0;
+if (b == NULL)
+	return (0);
+while (b[len])
+	len++;
+while (len)
+{
+	if (b[len - 1] != '0' && b[len - 1] != '1')
+		return (0);
+	if (b[len - 1] == '1')
+		result += base;
+	base *= 2;
+	len--;
+}
+return (result);
+}
